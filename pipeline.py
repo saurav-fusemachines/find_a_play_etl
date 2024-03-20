3f2516cd7ad91a097a9bb894af2c2410dd75c18f
saurav-fusemachines <saurav.karki@fusemachines.com>
2024-03-20 13:17:34 +0545

    pipeline.py file pushed

---
+++ pipeline.py
@@ -0,0 +1,7 @@
+from scraping_script.concord import start_concord
+from scraping_script.mti_scraping import start_mti
+from scraping_script.trw import start_trw
+
+# task1 = start_concord()
+task2 = start_mti()
+task3 = start_trw()
