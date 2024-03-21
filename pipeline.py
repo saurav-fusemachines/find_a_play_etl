b0feb7d0b17cd615674a48d8f179f595f8ea2f57
BIPIN-GHIMIRE-FUSEMACHINES <bipin.ghimire@fusemachines.com>
2024-03-21 15:58:09 +0545

    pipeline.py file changed

--- pipeline.py
+++ pipeline.py
@@ -12,18 +12,18 @@ from cleaning_script.concord_cleaning import cleaning_script_main as concord_cle
 def run_tasks():
     with concurrent.futures.ThreadPoolExecutor() as executor:
         # Start the tasks concurrently
-        scraping_concord = executor.submit(start_concord)
-        scraping_mti = executor.submit(start_mti)
-        scraping_trw = executor.submit(start_trw)
+        # scraping_concord = executor.submit(start_concord)
+        # scraping_mti = executor.submit(start_mti)
+        # scraping_trw = executor.submit(start_trw)
 
-        # Wait for all tasks to complete
-        concurrent.futures.wait([scraping_concord, scraping_mti, scraping_trw])
+        # # Wait for all tasks to complete
+        # concurrent.futures.wait([scraping_concord, scraping_mti, scraping_trw])
 
         cleaning_mti = executor.submit(mti_cleaning)
-        cleaning_trw = executor.submit(trw_cleaning)
+        # cleaning_trw = executor.submit(trw_cleaning)
         cleaning_concord = executor.submit(concord_cleaning)
 
-        concurrent.futures.wait([cleaning_mti, cleaning_trw, cleaning_concord])
+        concurrent.futures.wait([cleaning_mti, cleaning_concord])
         
 # Run the tasks
 run_tasks()
