63da029a90ce94b4a2afb6f7dca760a535f53915
BIPIN-GHIMIRE-FUSEMACHINES <bipin.ghimire@fusemachines.com>
2024-03-21 14:37:24 +0545

    pipeline.py: Added the script for cleaning trw site.

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,9 @@
 import concurrent.futures
-# from scraping_script.concord import start_concord
+from scraping_script.concord import start_concord
 from scraping_script.mti_scraping import start_mti
 from scraping_script.trw import start_trw
-from cleaning_script.mti_data_cleaning import cleaning_script_main
-
+from cleaning_script.mti_data_cleaning import cleaning_script_main as mti_cleaning
+from cleaning_script.trw_data_cleaning import cleaning_trw_main as trw_cleaning
 # task1 = start_concord()
 # task2 = start_mti()
 # task3 = start_trw()
@@ -11,16 +11,17 @@ from cleaning_script.mti_data_cleaning import cleaning_script_main
 def run_tasks():
     with concurrent.futures.ThreadPoolExecutor() as executor:
         # Start the tasks concurrently
-        # future1 = executor.submit(start_concord)
-        future2 = executor.submit(start_mti)
-        # future3 = executor.submit(start_trw)
+        scraping_concord = executor.submit(start_concord)
+        scraping_mti = executor.submit(start_mti)
+        scraping_trw = executor.submit(start_trw)
 
         # Wait for all tasks to complete
-        concurrent.futures.wait([future2])
+        concurrent.futures.wait([scraping_concord, scraping_mti, scraping_trw])
 
-        future3 = executor.submit(cleaning_script_main)
+        cleaning_mti = executor.submit(mti_cleaning)
+        cleaning_trw = executor.submit(trw_cleaning)
 
-        concurrent.futures.wait([future3])
+        concurrent.futures.wait([cleaning_mti, cleaning_trw])
 
 # Run the tasks
 run_tasks()
